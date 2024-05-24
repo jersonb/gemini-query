233a9533017126ab2e45e68791e66e153ea53ebf
jersonb <jersonlbrito@hotmail.com>
2024-05-24 17:37:20 -0300

    refactor: remove unused code

--- GeminiQuery.Mvc/Models/Question.cs
+++ GeminiQuery.Mvc/Models/Question.cs
@@ -2,10 +2,6 @@
 {
     public class Question
     {
-        public int Id { get; set; }
-
         public string Content { get; set; } = string.Empty!;
-
-        public string Query { get; set; } = string.Empty!;
     }
 }
